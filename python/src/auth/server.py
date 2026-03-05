f29828ba2f0d2742c932bff43a9fcf3a4b2874f5
BILLA <adithyen1@gmail.com>
2026-03-05 22:24:20 +0530

    final

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -1,7 +1,23 @@
 import jwt, datetime, os
 from flask import Flask, request
 from flask_mysqldb import MySQL
+from dotenv import load_dotenv
+
+load_dotenv()
 
 server = Flask(__name__)
 mysql = MySQL(server)
 
+server.config["MYSQL_HOST"] = os.getenv("MYSQL_HOST")
+server.config["MYSQL_USER"] = os.getenv("MYSQL_USER")
+server.config["MYSQL_PASSWORD"] = os.getenv("MYSQL_PASSWORD")
+server.config["MYSQL_DB"] = os.getenv("MYSQL_DB")
+server.config["MYSQL_PORT"] = os.getenv("MYSQL_PORT")
+
+@server.route("/login",methods = ["POST"])
+def login():
+    auth = request.authorization 
+    if not auth:
+        return "Missing credentials",401
+        
+        
